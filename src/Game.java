ae77adccea7b2c75b2c08b840f6d0e70a3290c82
Yufang <411422259@m365.fju.edu.tw>
2024-01-13 00:03:23 +0800

    完整版(尚未DEBUG)

--- src/Game.java
+++ src/Game.java
@@ -1,28 +1,34 @@
 import java.util.*;
-//猜單字(皆大寫)
+//猜單字(皆大寫) //備註:位置及字母皆正確，以*備註；僅字母正確，以'備註;甚麼都不是的話，不用備註
 public class Game {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		String alphabet[] = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+		char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
 		String word[]= {"APPLE","YELLOW","TRAIN","CHAIR","DEEAM","MUSIC","ELEPHANT","AIRPLANE","APARTMENT","AUTUMN","BEE","BIRTHDAY","BOOKSTORE","CAMERA","CHOCOLATE","COOKIE","DANCE","FRUIT","GLASS","LEMON"};
 		char[] QWORD; //存放題目
 		char[] AWORD; //存放答案
-		boolean ok=false;
+		boolean ok=false; boolean ok1=false;
 		
-		alphaprint(alphabet);//列出尚未選過的字母
+		
 		QWORD = Qword(word).toCharArray();//隨機挑取word裡的單字作為題目
-		Qwordprint(QWORD); //出題
-		do{ 
-			//輸入與底線相同數量字母的單字，如長度不同再輸一次。
-			AWORD = sc.nextLine().toCharArray();
-			ok = (QWORD.length==AWORD.length);
-			System.out.println(QWORD.length+" "+AWORD.length);
-		}while(!ok) ;
-		System.out.print("g");
+		for(int i=0;i<QWORD.length;i++)
+			System.out.print(QWORD[i]);
+		System.out.println();
+		
+		do {
+			alphaprint(alphabet);//列出尚可選的字母
+			Qwordprint(QWORD); //出題
+			do{ //輸入與底線相同數量字母的單字，如長度不同再輸一次。
+				AWORD = sc.nextLine().toCharArray();
+				ok = check(QWORD,AWORD,alphabet);
+			}while(!ok) ;
+			//System.out.print("g");
+			AQwordcompare(QWORD,AWORD,alphabet);
+		}while(check2(QWORD,AWORD));
 		
 		
 	}
-	public static void alphaprint(String a[]) {
+	public static void alphaprint(char a[]) {
 		for(int i=0;i<a.length;i++) {
 			System.out.print(a[i]+"   ");
 			if(i%9==8)
@@ -43,7 +49,46 @@ public class Game {
 			System.out.print("_ ");
 		System.out.println(":");
 	}
+	public static boolean check(char q[],char a[], char alpha[]) {
+		if(q.length!=a.length) //長度不相同，直接回傳false
+			return(false);
+		for(int i=0;i<a.length;i++)
+			for(int j=0;j<26;j++) {
+				if(a[i]==alpha[j]) //如果字母表裡目前還有此字母
+					return(true); //回傳 true
+			}
+		return(false); //如果迴圈完整跑完都沒發現有此字母，則回傳 false
+	}
+	public static boolean check2(char q[],char a[]) {
+		for(int i=0;i<a.length;i++) 
+			if(a[i]!=q[i])
+				return false;
+		return true;
+	}
+	
 	public static void AQwordcompare(char q[],char a[], char alpha[]){
-		
+		int l =q.length;
+		for(int i=0;i<l;i++) {
+			boolean ok = false;
+			for(int j=0;j<l;j++) {
+				if(a[i]==q[j]) {//當儲存格內容相同，代表數子猜對，最少得 1B
+					if(i==j)
+						System.out.print(a[i]+"* ");//位置及字母皆正確，以*備註。
+					else
+						System.out.print(a[i]+"' "); //僅字母正確，以'備註。
+					ok = true;
+					continue;
+				}
+				
+			}
+			if(!ok) {
+				System.out.print(a[i]+" "); //甚麼都不是的話，不用備註。
+				for(int r=0;r<26;r++) {
+					if(a[i]==alpha[r])
+						alpha[r]="-".charAt(0);continue;
+				}
+			}
+		}
+		System.out.println();
 	}
 }
